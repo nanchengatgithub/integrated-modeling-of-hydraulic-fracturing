1f022f152ab1dd849fe1b09f6e029d41a90951de
Nan Cheng <53907071+nanchengatgithub@users.noreply.github.com>
2024-03-25 16:25:30 +0100

    Update make_tar_file_from_filelist.sh

--- make_tarfile_for_HFM/make_tar_file_from_filelist.sh
+++ make_tarfile_for_HFM/make_tar_file_from_filelist.sh
@@ -1,6 +1,5 @@
 # to zip all the files listed in this file on Linux: 
 tar -czvf hfm_files.tar -T hfm_filelist.txt
 
-
 # to unzip the tar-file to an existing folder tmp: 
 #tar -xzvf hfm_files.tar -C tmp
